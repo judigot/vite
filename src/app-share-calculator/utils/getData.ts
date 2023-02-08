a667d84b752274609f980d60197fb704d3ea0265
judigot <judigot@github.com>
2023-02-09 02:41:53 +0800

    Added share calculator

---
+++ src/app-share-calculator/utils/getData.ts
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+(async () => {
+  const auth: boolean = await Authentication();
+
+  async function Authentication() {
+    return true;
+  }
+})();
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 
-import App from "./app-bootstrap";
+import App from "./app-share-calculator";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
