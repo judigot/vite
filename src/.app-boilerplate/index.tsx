f923766b312e1d286c7a7b8c59dfd164c8f31509
judigot <judigot@github.com>
2023-02-09 22:51:52 +0800

    Commit

--- src/.app-boilerplate/index.tsx
+++ src/.app-boilerplate/index.tsx
@@ -1,3 +1,1 @@
-import Default from "app/Controller";
-alert(Default());
 export { default } from "./AppBoilerplate";
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 const path = require("path");
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
