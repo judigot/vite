5241c7aafb132b318275df0fc3e6d5dae18be948
judigot <judigot@github.com>
2023-02-11 17:56:14 +0800

    Added children rendering

--- src/.app-boilerplate/AppBoilerplate.tsx
+++ src/.app-boilerplate/AppBoilerplate.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styled from "styled-components";
 
+import ChildComponent from "./components/ChildComponent";
 import Data from "./utils/getData";
 
 const H1Styled = styled.h1`
@@ -11,6 +12,7 @@ const H1Styled = styled.h1`
 interface Props {}
 
 export default function App(props: Props) {
+  const [count, setCount] = React.useState<number>(0);
   const [initialData, setInitialData] = React.useState<string>();
 
   React.useEffect(() => {
@@ -37,16 +39,24 @@ export default function App(props: Props) {
   //====================HELPER FUNCTIONS====================//
   // Function expression syntax to save memory
   const handleClick = () => {
-    alert("Waddup");
+    if (count !== 5) {
+      setCount(count + 1);
+    }
   };
   //====================HELPER FUNCTIONS====================//
 
   return (
     <>
-      <H1Styled>Styled Component</H1Styled>
+      <H1Styled>Styled Functional Component</H1Styled>
       <span>Initial Data:</span>
       <p>{JSON.stringify(initialData)}</p>
-      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick}>You've clicked {count} times.</button>
+
+      <div>
+        {[...Array(count)].map((element, i) => {
+          return <ChildComponent key={i} index={i + 1} />;
+        })}
+      </div>
     </>
-  );
+  ;
 }
