d58b0da67b586b4c5ff621d7ab4db8d364942af0
judigot <judigot@github.com>
2023-04-19 23:58:43 +0800

    Refactor

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   build: {
     target: "ESNext",
   },
@@ -19,5 +20,4 @@ export default defineConfig({
       },
     ],
   },
-  base: "./",
 });
