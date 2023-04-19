6b7764a3dc70effd0c3f646e5c1c17fa7dcb2ef3
judigot <judigot@github.com>
2023-04-19 18:06:04 +0800

    Refactor

--- src/app-react-router/ReactRouter.tsx
+++ src/app-react-router/ReactRouter.tsx
@@ -43,7 +43,7 @@ export default function App() {
       },
       {
         path: "/user/:userID",
-        element: <>{<ProfilePage />}</>,
+        element: <ProfilePage />,
       },
     ],
     {
