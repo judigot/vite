dfb320d22925164e68662e9340de81c0a502c677
judigot <judigot@github.com>
2023-10-19 18:43:06 +0800

    feat: add sample test

---
+++ src/app-test/App.test.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import App from "./App";
+
+import "@testing-library/jest-dom";
+
+import { act, renderHook, render, screen } from "@testing-library/react";
+
+const useCustomHook = ({ propVal }: { propVal: string }) => {
+  const [name, setName] = React.useState<string>(propVal);
+
+  const mutateData = async (mutationValue: string) => {
+    await fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then(() => {
+        setName(mutationValue);
+      });
+  };
+  return { name, mutateData };
+};
+
+describe("Tester", () => {
+  it("test", async () => {
+    const { result } = renderHook(useCustomHook, {
+      initialProps: { propVal: "Old Name" },
+    });
+
+    console.log(result.current.name);
+
+    expect(result.current.name).toBe("Old Name");
+
+    await act(async () => result.current.mutateData("New Name"));
+
+    expect(result.current.name).toBe("New Name");
+
+    console.log(result.current.name);
+
+    const props = { isPasswordCorrect: true };
+    render(<App isPasswordCorrect={props.isPasswordCorrect} />);
+    const component = screen.getByTestId("admin-container");
+    expect(component).toBeInTheDocument();
+  });
+});
---
+++ src/app-test/App.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface Props {
+  isPasswordCorrect?: boolean;
+}
+
+function Admin() {
+  return (
+    <div data-testid="admin-container">
+      <span>Hello, Admin</span>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div data-testid="home-container">Do you have an account? Log in!</div>
+  );
+}
+
+export default function App({ isPasswordCorrect }: Props) {
+  const [isAuth, setIsAuth] = React.useState<boolean>(isPasswordCorrect ?? false);
+  return (
+    <div style={{ width: "min-content" }}>
+      <section className="layer-desk">
+        {isAuth && <Admin />}
+        {!isAuth && <Home />}
+        <button
+          type="button"
+          onClick={() => {
+            setIsAuth(!isAuth);
+          }}
+        >
+          {isAuth && <>Log Out</>}
+          {!isAuth && <>Log In</>}
+        </button>
+      </section>
+    </div>
+  );
+}
